a255cd39a03d989eab7d20a02d337377377d716d
Damien Tan <74666853+DamienTan01@users.noreply.github.com>
2023-07-24 20:31:09 +0800

    Update login.dart

--- groomify/lib/pages/login.dart
+++ groomify/lib/pages/login.dart
@@ -10,8 +10,121 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController _emailTextController = TextEditingController();
+  TextEditingController _passwordTextController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Stack(
+      children: [
+        BackgroundImage(),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 60),
+                  child: Container(
+                    height: 200,
+                    child: Center(
+                      child: Text(
+                        'Groomify',
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: Column(
+                    children: [
+                      Container(
+                        child: forTextField(
+                            "Email", Icons.email, false, _emailTextController),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Container(
+                        child: forTextField("Password", Icons.lock, true,
+                            _passwordTextController),
+                      ),
+                      SizedBox(
+                        height: 80,
+                      ),
+
+                      //Login Button
+                      Container(
+                        height: 45,
+                        width: 250,
+                        child: ElevatedButton.icon(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(0xFF731942)),
+                          onPressed: () {
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return Center(
+                                      child: CircularProgressIndicator());
+                                });
+
+                            FirebaseAuth.instance
+                                .signInWithEmailAndPassword(
+                                email: _emailTextController.text,
+                                password: _passwordTextController.text)
+                                .then((value) {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => HomePage(
+                                        comicList: [],
+                                        novelList: [],
+                                      )));
+                            }).onError((error, stackTrace) {
+                              final snackbar = SnackBar(
+                                content: const Text(
+                                    "Sorry, Invalid Email or Password.."),
+                                action: SnackBarAction(
+                                    label: 'OK', onPressed: () {}),
+                              );
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(snackbar);
+                            });
+                          },
+                          icon: Icon(
+                            Icons.login,
+                          ),
+                          label: Text(
+                            'Sign In',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                        ), //Need to add button
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton.extended(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => SignUpPage()));
+            },
+            label: const Text("Sign Up Here"),
+            backgroundColor: Color(0xFF731942),
+            icon: const Icon(
+              Icons.app_registration,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
