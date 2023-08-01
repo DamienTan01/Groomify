4a5282a6b1968c8729badba6262e742c325d9c0d
Damien Tan <74666853+DamienTan01@users.noreply.github.com>
2023-08-01 22:53:06 +0800

    Update login.dart

--- groomify/lib/pages/login.dart
+++ groomify/lib/pages/login.dart
@@ -1,130 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class loginPage extends StatefulWidget {
+  const loginPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<loginPage> createState() => _loginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
-  TextEditingController _emailTextController = TextEditingController();
-  TextEditingController _passwordTextController = TextEditingController();
-
+class _loginPageState extends State<loginPage> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        BackgroundImage(),
-        Scaffold(
-          backgroundColor: Colors.transparent,
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 60),
-                  child: Container(
-                    height: 200,
-                    child: Center(
-                      child: Text(
-                        'Groomify',
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: Column(
-                    children: [
-                      Container(
-                        child: forTextField(
-                            "Email", Icons.email, false, _emailTextController),
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Container(
-                        child: forTextField("Password", Icons.lock, true,
-                            _passwordTextController),
-                      ),
-                      SizedBox(
-                        height: 80,
-                      ),
-
-                      //Login Button
-                      Container(
-                        height: 45,
-                        width: 250,
-                        child: ElevatedButton.icon(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Color(0xFF731942)),
-                          onPressed: () {
-                            showDialog(
-                                context: context,
-                                builder: (context) {
-                                  return Center(
-                                      child: CircularProgressIndicator());
-                                });
-
-                            FirebaseAuth.instance
-                                .signInWithEmailAndPassword(
-                                email: _emailTextController.text,
-                                password: _passwordTextController.text)
-                                .then((value) {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => HomePage(
-                                        comicList: [],
-                                        novelList: [],
-                                      )));
-                            }).onError((error, stackTrace) {
-                              final snackbar = SnackBar(
-                                content: const Text(
-                                    "Sorry, Invalid Email or Password.."),
-                                action: SnackBarAction(
-                                    label: 'OK', onPressed: () {}),
-                              );
-                              ScaffoldMessenger.of(context)
-                                  .showSnackBar(snackbar);
-                            });
-                          },
-                          icon: Icon(
-                            Icons.login,
-                          ),
-                          label: Text(
-                            'Sign In',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w900,
-                            ),
-                          ),
-                        ), //Need to add button
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          floatingActionButton: FloatingActionButton.extended(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => SignUpPage()));
-            },
-            label: const Text("Sign Up Here"),
-            backgroundColor: Color(0xFF731942),
-            icon: const Icon(
-              Icons.app_registration,
-            ),
-          ),
-        ),
-      ],
-    );
+    return const Placeholder();
   }
 }
