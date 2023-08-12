8072af31a14c913ccd911ca0fbb18c822d606053
Damien Tan <74666853+DamienTan01@users.noreply.github.com>
2023-08-12 16:28:00 +0800

    added source code

---
+++ Groomify/android/app/src/main/kotlin/com/example/groomify/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.groomify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Groomify/lib/pages/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:groomify/pages/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Groomify',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xffF7D1CD),
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
