c0f5cc97ccf7cdc68338703e8fd362a84ab87d0a
Damien Tan <74666853+DamienTan01@users.noreply.github.com>
2023-09-16 22:53:20 +0800

    Added alert message prompt

--- Groomify/lib/controller/auth_controller.dart
+++ Groomify/lib/controller/auth_controller.dart
@@ -31,59 +31,92 @@ class AuthController extends GetxController {
   //Navigate user to HomePage
   _initialScreen(User? user) {
     //Navigate user to LoginPage if the user is not logged in
-    if(user==null) {
+    if (user == null) {
       print("Login Page");
-      Get.offAll(()=>LoginPage());
-    }else {
-      Get.offAll(()=>HomePage(email:user.email!));
+      Get.offAll(() => LoginPage());
+    } else {
+      Get.offAll(() => HomePage(email: user.email!));
     }
   }
 
   //Function for registration
-  void register(String email, password) async{
+  void register(String email, password) async {
     //Try/catch is used for exceptions when things go wrong
     try {
-      await auth.createUserWithEmailAndPassword(email: email, password: password);
-      Future addDetails(String uid, String username, String email) async {
-        await FirebaseFirestore.instance.collection('users').add({
-          'uid': uid,
-          'username': username,
-          'email': email,
-        });
-      }
-    }catch(e) {
+      await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      // Future addDetails(String uid, String username, String email) async {
+      //   await FirebaseFirestore.instance.collection('users').add({
+      //     'uid': uid,
+      //     'username': username,
+      //     'email': email,
+      //   });
+      // }
+    } catch (e) {
       //Prompt a message when registration failed
-      Get.snackbar("About User", "User Message",
-        backgroundColor: Colors.redAccent,
-        snackPosition: SnackPosition.BOTTOM,
+      Get.snackbar(
+        "Groomify",
+        "Account Creation Failed",
         titleText: Text(
-          "Account Creation Failed",
+            "Groomify",
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+                color: Colors.white
+            )
         ),
+        messageText: Text(
+          "Account Creation Failed",
+          // textAlign: TextAlign.center,
+          style: TextStyle(
+              fontSize: 15,
+              color: Colors.white
+          ),
+        ),
+        icon: const Icon(Icons.add_alert),
+        backgroundColor: Color(0xff735D78),
+        margin: EdgeInsets.all(15),
+        duration: Duration(seconds: 3),
+        snackPosition: SnackPosition.BOTTOM,
       );
     }
   }
-  void login(String email, password) async{
+
+  void login(String email, password) async {
     //Try/catch is used for exceptions when things go wrong
     try {
       await auth.signInWithEmailAndPassword(email: email, password: password);
-    }catch(e) {
-      //Prompt a message when registration failed
-      Get.snackbar("About Login", "Empty Fields",
-          backgroundColor: Colors.redAccent,
-          snackPosition: SnackPosition.BOTTOM,
-          titleText: Text(
-            "Login Failed",
+    } catch (e) {
+      //Prompt a message when login failed
+      Get.snackbar(
+        "Groomify",
+        "Login Failed",
+        titleText: Text(
+            "Groomify",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+              color: Colors.white
+            )
+        ),
+        messageText: Text(
+          "Login Failed",
+          // textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 15,
+            color: Colors.white
           ),
-          // messageText: Text(
-          //   e.toString(),
-          //   style: TextStyle(
-          //       color: Colors.white
-          //   ),
-          // )
+        ),
+        icon: const Icon(Icons.add_alert),
+        backgroundColor: Color(0xff735D78),
+        margin: EdgeInsets.all(15),
+        duration: Duration(seconds: 3),
+        snackPosition: SnackPosition.BOTTOM,
       );
     }
   }
-  void logout() async{
+
+  void logout() async {
     await auth.signOut();
   }
 }
