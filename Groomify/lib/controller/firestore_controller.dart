04e6c9db4ff3a7c60c5f355822264226995b1cad
Damien Tan <74666853+DamienTan01@users.noreply.github.com>
2023-09-18 00:07:02 +0800

    Created splashscreen

--- Groomify/lib/controller/firestore_controller.dart
+++ Groomify/lib/controller/firestore_controller.dart
@@ -1,8 +1,1 @@
-class FirestoreController extends StatelessWidget {
-  const FirestoreController({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return ;
-  }
-}
--- Groomify/lib/pages/main.dart
+++ Groomify/lib/pages/main.dart
@@ -1,3 +1,5 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:flutter/material.dart';
 import 'package:groomify/controller/auth_controller.dart';
 import 'package:groomify/pages/login.dart';
@@ -24,7 +26,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Color(0xffF7D1CD),
         primarySwatch: Colors.blue,
       ),
-      home: const LoginPage(),
+      home: LoginPage(),
     );
   }
 }
