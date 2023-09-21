a57d2db5ec350ebfe5b61be7f4d7458be9c0f972
Damien Tan <74666853+DamienTan01@users.noreply.github.com>
2023-09-21 09:19:52 +0800

    create btmnavbar

--- Groomify/lib/pages/home.dart
+++ Groomify/lib/pages/home.dart
@@ -15,51 +15,57 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: [
             //Button
-            GestureDetector(
-              onTap: () {
-                AuthController.instance.logout();
-              },
-              child: Container(
-                margin: EdgeInsets.only(top: 100),
-                width: w * 0.3,
-                height: h * 0.06,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Color(0xff735D78),
-                    boxShadow: [
-                      BoxShadow(
-                          blurRadius: 3, offset: Offset(2, 3), color: Colors.grey)
-                    ]),
-                child: Center(
-                  child: Text(
-                    "Logout",
-                    style: TextStyle(
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ),
+            // GestureDetector(
+            //   onTap: () {
+            //     AuthController.instance.logout();
+            //   },
+            //   child: Container(
+            //     margin: EdgeInsets.only(top: 100),
+            //     width: w * 0.3,
+            //     height: h * 0.06,
+            //     decoration: BoxDecoration(
+            //         borderRadius: BorderRadius.circular(10),
+            //         color: Color(0xff735D78),
+            //         boxShadow: [
+            //           BoxShadow(
+            //               blurRadius: 3, offset: Offset(2, 3), color: Colors.grey)
+            //         ]),
+            //     child: Center(
+            //       child: Text(
+            //         "Logout",
+            //         style: TextStyle(
+            //           fontSize: 35,
+            //           fontWeight: FontWeight.bold,
+            //           color: Colors.white,
+            //         ),
+            //       ),
+            //     ),
+            //   ),
+            // ),
+
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xffD1B3C4),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        // currentIndex: _selectedIndex,
+        selectedItemColor: Color(0xff735D78),
+        // onTap: _onItemTapped,
       ),
     );
   }
 }
-
-class NavBar extends StatefulWidget {
-  const NavBar({super.key});
-
-  @override
-  State<NavBar> createState() => _NavBarState();
-}
-
-class _NavBarState extends State<NavBar> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
-
