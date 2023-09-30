831252ec6fbd910d15e615bc40878a09cfd59cbe
Damien Tan <74666853+DamienTan01@users.noreply.github.com>
2023-09-30 16:06:18 +0800

    Created recommended groomers

--- Groomify/lib/pages/home.dart
+++ Groomify/lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:groomify/controller/auth_controller.dart';
 import 'package:groomify/pages/btmNavBar.dart';
@@ -54,7 +55,7 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Column(
         children: [
-          SizedBox(height: 20),
+          SizedBox(height: 30),
           //Search Bar
           Center(
             child: Container(
@@ -82,7 +83,7 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
-          SizedBox(height: 20),
+          SizedBox(height: 30),
           //Text
           Column(
             children: [
@@ -108,69 +109,87 @@ class _HomePageState extends State<HomePage> {
               ),
               SizedBox(height: 20),
               //Services
+              GridView.count(
+                crossAxisCount: 4,
+                shrinkWrap: true,
+                children: [
+                  _buildServiceButton('Pet Bathing', Icons.bathtub),
+                  _buildServiceButton('Haircuts', Icons.content_cut),
+                  _buildServiceButton('Nail Trim', Icons.content_paste),
+                  _buildServiceButton('Teeth Clean', Icons.brush),
+                  _buildServiceButton('Ear Clean', Icons.hearing),
+                  _buildServiceButton('Flea & Tick Treatment', Icons.bug_report),
+                  _buildServiceButton('Anal Gland Expression', Icons.face),
+                  _buildServiceButton('Paw Pad Care', Icons.pets),
+                ],
+              ),
+              SizedBox(height: 30),
+              //Text
               Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      child: IconButton(
-                        icon: Icon(Icons.pets_outlined),
-                        onPressed: () {
-                          // Add your onPressed logic here
-                        },
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(left: 15),
+                child: Text(
+                  'Recommended Groomers',
+                  style: TextStyle(
+                    fontSize: 27,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    shadows: [
+                      Shadow(
+                        offset: Offset(2, 2),
+                        blurRadius: 3.0,
+                        color: Colors.grey.withOpacity(0.5),
                       ),
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.search),
-                      onPressed: () {
-                        // Add your onPressed logic here
-                      },
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.favorite),
-                      onPressed: () {
-                        // Add your onPressed logic here
-                      },
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.settings),
-                      onPressed: () {
-                        // Add your onPressed logic here
-                      },
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-              SizedBox(height: 20),
+              SizedBox(height: 30),
+              //Horizontal scroll
+              // Replace this part of your code with the horizontal scroll view
               Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconButton(
-                      icon: Icon(Icons.home),
-                      onPressed: () {
-                        // Add your onPressed logic here
-                      },
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.search),
-                      onPressed: () {
-                        // Add your onPressed logic here
-                      },
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.favorite),
-                      onPressed: () {
-                        // Add your onPressed logic here
-                      },
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.settings),
-                      onPressed: () {
-                        // Add your onPressed logic here
-                      },
-                    ),
-                  ],
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: List.generate(5, (index) {
+                      return GestureDetector(
+                        onTap: () {
+                          // Handle the click event here, e.g., navigate to a page
+                          // when one of the items is clicked
+                          // You can use a switch statement or if-else to determine
+                          // which item was clicked based on the 'index' variable.
+                          switch (index) {
+                            case 0:
+                            // Handle the click for the first item
+                              break;
+                            case 1:
+                            // Handle the click for the second item
+                              break;
+                          // Add cases for the other items as needed
+                          }
+                        },
+                        child: Container(
+                          width: 150,
+                          height: 150,// Set the width of each item
+                          margin: EdgeInsets.symmetric(horizontal: 10), // Add horizontal margin
+                          decoration: BoxDecoration(
+                            color: Color(0xffD1B3C4),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Item ${index + 1}',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+                  ),
                 ),
               ),
             ],
@@ -184,3 +203,28 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+Widget _buildServiceButton(String label, IconData iconData) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Icon(
+        iconData,
+        size: 48, // Adjust the icon size as needed
+        color: Colors.black, // Adjust the icon color as needed
+      ),
+      SizedBox(height: 8), // Add spacing between icon and label
+      Text(
+        label,
+        style: TextStyle(
+          fontSize: 16, // Adjust the label font size as needed
+          fontWeight: FontWeight.bold,
+          color: Colors.black,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    ],
+  );
+}
+
+
