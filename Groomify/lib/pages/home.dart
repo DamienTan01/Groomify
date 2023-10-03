56490d9260df21b1995d17339f5e6c7cf536d5df
Damien Tan <74666853+DamienTan01@users.noreply.github.com>
2023-10-04 00:09:15 +0800

    Update home.dart

--- Groomify/lib/pages/home.dart
+++ Groomify/lib/pages/home.dart
@@ -53,159 +53,161 @@ class _HomePageState extends State<HomePage> {
         ],
         elevation: 0,
       ),
-      body: Column(
-        children: [
-          SizedBox(height: 30),
-          //Search Bar
-          Center(
-            child: Container(
-              width: 350,
-              height: 50, // Set the height of the search bar
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                    offset: Offset(0, 3),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 30),
+            //Search Bar
+            Center(
+              child: Container(
+                width: 350,
+                height: 50, // Set the height of the search bar
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIconColor: Color(0xff735D78),
+                    prefixIcon: Icon(Icons.search),
+                    hintText: 'Search for Groomers',
+                    border: InputBorder.none,
                   ),
-                ],
-              ),
-              child: TextField(
-                decoration: InputDecoration(
-                  prefixIconColor: Color(0xff735D78),
-                  prefixIcon: Icon(Icons.search),
-                  hintText: 'Search for Groomers',
-                  border: InputBorder.none,
                 ),
               ),
             ),
-          ),
-          SizedBox(height: 30),
-          //Text
-          Column(
-            children: [
-              //Text
-              Container(
-                alignment: Alignment.centerLeft,
-                padding: EdgeInsets.only(left: 15),
-                child: Text(
-                  'Grooming Services',
-                  style: TextStyle(
-                    fontSize: 27,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                    shadows: [
-                      Shadow(
-                        offset: Offset(2, 2),
-                        blurRadius: 3.0,
-                        color: Colors.grey.withOpacity(0.5),
-                      ),
-                    ],
+            SizedBox(height: 30),
+            //Text
+            Column(
+              children: [
+                //Text
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.only(left: 15),
+                  child: Text(
+                    'Grooming Services',
+                    style: TextStyle(
+                      fontSize: 27,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      shadows: [
+                        Shadow(
+                          offset: Offset(2, 2),
+                          blurRadius: 3.0,
+                          color: Colors.grey.withOpacity(0.5),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
-              //Services
-              GridView.count(
-                crossAxisCount: 4,
-                shrinkWrap: true,
-                children: [
-                  _buildServiceButton('Pet Bathing', Icons.bathtub),
-                  _buildServiceButton('Haircuts', Icons.content_cut),
-                  _buildServiceButton('Nail Trim', Icons.content_paste),
-                  _buildServiceButton('Teeth Clean', Icons.brush),
-                  _buildServiceButton('Ear Clean', Icons.hearing),
-                  _buildServiceButton('Flea & Tick Treatment', Icons.bug_report),
-                  _buildServiceButton('Anal Gland Expression', Icons.face),
-                  _buildServiceButton('Paw Pad Care', Icons.pets),
-                ],
-              ),
-              SizedBox(height: 30),
-              //Text
-              Container(
-                alignment: Alignment.centerLeft,
-                padding: EdgeInsets.only(left: 15),
-                child: Text(
-                  'Recommended Groomers',
-                  style: TextStyle(
-                    fontSize: 27,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                    shadows: [
-                      Shadow(
-                        offset: Offset(2, 2),
-                        blurRadius: 3.0,
-                        color: Colors.grey.withOpacity(0.5),
-                      ),
-                    ],
+                SizedBox(height: 20),
+                //Services
+                GridView.count(
+                  crossAxisCount: 4,
+                  shrinkWrap: true,
+                  children: [
+                    _buildServiceButton('Pet Bathing', Icons.bathtub),
+                    _buildServiceButton('Haircuts', Icons.content_cut),
+                    _buildServiceButton('Nail Trim', Icons.content_paste),
+                    _buildServiceButton('Teeth Clean', Icons.brush),
+                    _buildServiceButton('Ear Clean', Icons.hearing),
+                    _buildServiceButton('Flea & Tick Treatment', Icons.bug_report),
+                    _buildServiceButton('Anal Gland Expression', Icons.face),
+                    _buildServiceButton('Paw Pad Care', Icons.pets),
+                  ],
+                ),
+                SizedBox(height: 30),
+                //Text
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.only(left: 15),
+                  child: Text(
+                    'Recommended Groomers',
+                    style: TextStyle(
+                      fontSize: 27,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      shadows: [
+                        Shadow(
+                          offset: Offset(2, 2),
+                          blurRadius: 3.0,
+                          color: Colors.grey.withOpacity(0.5),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 30),
-              //Horizontal scroll
-              Container(
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: List.generate(5, (index) {
-                      return GestureDetector(
-                        onTap: () {
-                          // Handle the click event here, e.g., navigate to a page
-                          // when one of the items is clicked
-                          // You can use a switch statement or if-else to determine
-                          // which item was clicked based on the 'index' variable.
-                          switch (index) {
-                            case 0:
-                            // Handle the click for the first item
-                              break;
-                            case 1:
-                            // Handle the click for the second item
-                              break;
-                          // Add cases for the other items as needed
-                          }
-                        },
-                        child: Column(
-                          children: [
-                            Container(
-                              width: 150,
-                              height: 150, // Set the width of each item
-                              margin: EdgeInsets.symmetric(horizontal: 10), // Add horizontal margin
-                              decoration: BoxDecoration(
-                                color: Color(0xffD1B3C4),
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  'Item ${index + 1}',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black,
+                SizedBox(height: 30),
+                //Horizontal scroll
+                Container(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: List.generate(5, (index) {
+                        return GestureDetector(
+                          onTap: () {
+                            // Handle the click event here, e.g., navigate to a page
+                            // when one of the items is clicked
+                            // You can use a switch statement or if-else to determine
+                            // which item was clicked based on the 'index' variable.
+                            switch (index) {
+                              case 0:
+                              // Handle the click for the first item
+                                break;
+                              case 1:
+                              // Handle the click for the second item
+                                break;
+                            // Add cases for the other items as needed
+                            }
+                          },
+                          child: Column(
+                            children: [
+                              Container(
+                                width: 150,
+                                height: 150, // Set the width of each item
+                                margin: EdgeInsets.symmetric(horizontal: 10), // Add horizontal margin
+                                decoration: BoxDecoration(
+                                  color: Color(0xffD1B3C4),
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    'Item ${index + 1}',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                            SizedBox(height: 10), // Space between item and description
-                            Text(
-                              'Description for Item ${index + 1}',
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.black,
+                              SizedBox(height: 10), // Space between item and description
+                              Text(
+                                'Description for Item ${index + 1}',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.black,
+                                ),
                               ),
-                            ),
-                          ],
-                        ),
-                      );
-                    }),
+                            ],
+                          ),
+                        );
+                      }),
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: CustomNavBar(
         selectedIndex: _selectedIndex,
