7e55d6d95911def2adbf88783b01180043306d8b
Damien Tan <74666853+DamienTan01@users.noreply.github.com>
2023-10-15 13:12:49 +0800

    Update firestore_controller.dart

--- Groomify/lib/controller/firestore_controller.dart
+++ Groomify/lib/controller/firestore_controller.dart
@@ -1,8 +1,5 @@
-import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:path_provider/path_provider.dart';
 
 class FirestoreController {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
