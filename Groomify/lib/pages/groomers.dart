c0c1b60cd38e710eff43a5bb7fdceffd3089d4c5
Damien Tan <74666853+DamienTan01@users.noreply.github.com>
2023-10-15 13:36:07 +0800

    update

--- Groomify/lib/pages/groomers.dart
+++ Groomify/lib/pages/groomers.dart
@@ -6,7 +6,38 @@ class GroomerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(
+          'Groomify',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 27,
+            fontWeight: FontWeight.bold,
+            shadows: [
+              Shadow(
+                offset: Offset(2, 2),
+                blurRadius: 3.0,
+                color: Colors.grey.withOpacity(0.5),
+              ),
+            ],
+          ),
+        ),
+        backgroundColor: Color(0xffD1B3C4),
+        actions: [
+          //Log out Button
+          IconButton(
+              icon: Icon(Icons.logout),
+              iconSize: 35,
+              color: Color(0xff735D78),
+              onPressed: () {
+                AuthController.instance.logout();
+              }
+          )
+        ],
+        elevation: 0,
+      ),
+
     );
   }
 }
