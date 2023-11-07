87a0a7782b472c3258661ee025da5352cade7a00
Damien Tan <74666853+DamienTan01@users.noreply.github.com>
2023-11-07 13:33:55 +0800

    Update home.dart

--- Groomify/lib/pages/home.dart
+++ Groomify/lib/pages/home.dart
@@ -104,189 +104,189 @@ class _HomePageState extends State<HomePage> {
         ],
         elevation: 0,
       ),
-      body: Column(
-        children: [
-          // Text
-          Column(
-            children: [
-              const SizedBox(height: 20),
-              // Text
-              Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.only(left: 15),
-                child: Text(
-                  'Appointments',
-                  style: TextStyle(
-                    fontSize: 27,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                    shadows: [
-                      Shadow(
-                        offset: const Offset(2, 2),
-                        blurRadius: 3.0,
-                        color: Colors.grey.withOpacity(0.5),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              // Appointments ListView
-              SingleChildScrollView(
-                child: Column(
-                  children: [
-                    ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: appointmentData.length > 2 ? 2 : appointmentData.length, // Show a maximum of 2 appointments
-                      itemBuilder: (context, index) {
-                        final appointment = appointmentData[index];
-                        final selectedDate = appointment['selectedDate'];
-
-                        // Format the date to "day, month, year" format
-                        final formattedDate = DateFormat('d MMMM y').format(selectedDate.toDate());
-
-                        return Container(
-                          margin: const EdgeInsets.all(10),
-                          padding: const EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '${appointment['salonName']}',
-                                style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                              ),
-                              const SizedBox(height: 10),
-                              Row(
-                                children: [
-                                  const Text(
-                                    'Date: ',
-                                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                                  ),
-                                  Text(
-                                    formattedDate, // Use the formatted date here
-                                    style: const TextStyle(fontSize: 18),
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(height: 5),
-                              Row(
-                                children: [
-                                  const Text(
-                                    'Time: ',
-                                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                                  ),
-                                  Text(
-                                    '${appointment['selectedTime']}',
-                                    style: const TextStyle(fontSize: 18),
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(height: 5),
-                              Row(
-                                children: [
-                                  const Text(
-                                    'Services: ',
-                                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                                  ),
-                                  Text(
-                                    '${appointment['selectedServices'].join(', ')}',
-                                    style: const TextStyle(fontSize: 18),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        );
-                      },
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Text
+            Column(
+              children: [
+                const SizedBox(height: 20),
+                // Text
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Text(
+                    'Appointments',
+                    style: TextStyle(
+                      fontSize: 27,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      shadows: [
+                        Shadow(
+                          offset: const Offset(2, 2),
+                          blurRadius: 3.0,
+                          color: Colors.grey.withOpacity(0.5),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 40),
-              // Text
-              Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.only(left: 15),
-                child: Text(
-                  'Recommended Groomers',
-                  style: TextStyle(
-                    fontSize: 27,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                    shadows: [
-                      Shadow(
-                        offset: const Offset(2, 2),
-                        blurRadius: 3.0,
-                        color: Colors.grey.withOpacity(0.5),
-                      ),
-                    ],
                   ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              // Horizontal scroll
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: List.generate(5, (index) {
-                    return GestureDetector(
-                      onTap: () {
-                        // Handle the click event here, e.g., navigate to a page
-                        // when one of the items is clicked
-                        // You can use a switch statement or if-else to determine
-                        // which item was clicked based on the 'index' variable.
-                        switch (index) {
-                          case 0:
-                          // Handle the click for the first item
-                            break;
-                          case 1:
-                          // Handle the click for the second item
-                            break;
-                        // Add cases for the other items as needed
-                        }
-                      },
-                      child: Column(
-                        children: [
-                          Container(
-                            width: 150,
-                            height: 150, // Set the width of each item
-                            margin: const EdgeInsets.symmetric(horizontal: 10), // Add horizontal margin
-                            decoration: BoxDecoration(
-                              color: const Color(0xffD1B3C4),
-                              borderRadius: BorderRadius.circular(20),
+                const SizedBox(height: 10),
+                // Appointments ListView
+                SizedBox(
+                  height: 300,
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    physics: const AlwaysScrollableScrollPhysics(),
+                    itemCount: appointmentData.length, // Show a maximum of 2 appointments
+                    itemBuilder: (context, index) {
+                      final appointment = appointmentData[index];
+                      final selectedDate = appointment['selectedDate'];
+
+                      // Format the date to "day, month, year" format
+                      final formattedDate = DateFormat('d MMMM y').format(selectedDate.toDate());
+
+                      return Container(
+                        margin: const EdgeInsets.all(10),
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '${appointment['salonName']}',
+                              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                             ),
-                            child: Center(
-                              child: Text(
-                                'Item ${index + 1}',
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
+                            const SizedBox(height: 10),
+                            Row(
+                              children: [
+                                const Text(
+                                  'Date: ',
+                                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  formattedDate, // Use the formatted date here
+                                  style: const TextStyle(fontSize: 18),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 5),
+                            Row(
+                              children: [
+                                const Text(
+                                  'Time: ',
+                                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  '${appointment['selectedTime']}',
+                                  style: const TextStyle(fontSize: 18),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 5),
+                            Row(
+                              children: [
+                                const Text(
+                                  'Services: ',
+                                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  '${appointment['selectedServices'].join(', ')}',
+                                  style: const TextStyle(fontSize: 18),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(height: 40),
+                // Text
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Text(
+                    'Recommended Groomers',
+                    style: TextStyle(
+                      fontSize: 27,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      shadows: [
+                        Shadow(
+                          offset: const Offset(2, 2),
+                          blurRadius: 3.0,
+                          color: Colors.grey.withOpacity(0.5),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                // Horizontal scroll
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: List.generate(5, (index) {
+                      return GestureDetector(
+                        onTap: () {
+                          // Handle the click event here, e.g., navigate to a page
+                          // when one of the items is clicked
+                          // You can use a switch statement or if-else to determine
+                          // which item was clicked based on the 'index' variable.
+                          switch (index) {
+                            case 0:
+                            // Handle the click for the first item
+                              break;
+                            case 1:
+                            // Handle the click for the second item
+                              break;
+                          // Add cases for the other items as needed
+                          }
+                        },
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 150,
+                              height: 150, // Set the width of each item
+                              margin: const EdgeInsets.symmetric(horizontal: 10), // Add horizontal margin
+                              decoration: BoxDecoration(
+                                color: const Color(0xffD1B3C4),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Item ${index + 1}',
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                          const SizedBox(height: 10), // Space between item and description
-                          Text(
-                            'Description for Item ${index + 1}',
-                            style: const TextStyle(
-                              fontSize: 16,
-                              color: Colors.black,
+                            const SizedBox(height: 10), // Space between item and description
+                            Text(
+                              'Description for Item ${index + 1}',
+                              style: const TextStyle(
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    );
-                  }),
+                          ],
+                        ),
+                      );
+                    }),
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: CustomNavBar(
         selectedIndex: _selectedIndex,
