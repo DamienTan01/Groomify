dab1c84f46978fd95779b8ca8752c62efaea2988
Damien Tan <74666853+DamienTan01@users.noreply.github.com>
2023-11-09 21:58:08 +0800

    update

--- Groomify/lib/pages/home.dart
+++ Groomify/lib/pages/home.dart
@@ -7,7 +7,6 @@ import 'package:groomify/pages/groomer_details.dart';
 import 'package:groomify/pages/groomers.dart';
 import 'package:groomify/pages/profile.dart';
 import 'package:intl/intl.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
