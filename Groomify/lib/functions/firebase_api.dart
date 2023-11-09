84034e3dfef03830ac31deabb067a714c0360a4c
Damien Tan <74666853+DamienTan01@users.noreply.github.com>
2023-11-09 22:03:30 +0800

    Update firebase_api.dart

--- Groomify/lib/functions/firebase_api.dart
+++ Groomify/lib/functions/firebase_api.dart
@@ -1,2 +1,20 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 
+class FirebaseApi {
+  // Create instance of firebase messaging
+  final _firebaseMessaging = FirebaseMessaging.instance;
+
+  // Function to initialize notifications
+  Future<void> initNotifications() async {
+    // Request permission from user (Will prompt user)
+    await _firebaseMessaging.requestPermission();
+
+    // Fetch the FCM token for this device
+    final fCMToken = await _firebaseMessaging.getToken();
+
+    // Print the token
+    print('Token: $fCMToken');
+  }
+
+
+}
